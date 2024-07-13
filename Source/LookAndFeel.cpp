ecc6d4f8d1ff96cd7d37fbd4ce56469350a542a6
Maciel <macielmick@gmail.com>
2024-07-13 16:33:06 +0100

    Update LookAndFeel.cpp

--- Source/LookAndFeel.cpp
+++ Source/LookAndFeel.cpp
@@ -2,16 +2,81 @@
 
 KnobLookAndFeel::KnobLookAndFeel()
 {
+    setColour(juce::Label::textColourId, Colors::Knob::label);
+    setColour(juce::Slider::textBoxTextColourId, Colors::Knob::label);
 }
 
 KnobLookAndFeel::~KnobLookAndFeel()
 {
 }
 
-void KnobLookAndFeel::drawRotarySlider(juce::Graphics& g, int x, int y, int width, int height,
-                                       float sliderPosProportional, float rotaryStartAngle, float rotaryEndAngle,
+void KnobLookAndFeel::drawRotarySlider(juce::Graphics& g,
+                                       int x, int y, int width, int height,
+                                       float sliderPosProportional, 
+                                       float rotaryStartAngle,
+                                       float rotaryEndAngle,
                                        juce::Slider& slider)
 {
+    auto bounds = juce::Rectangle<int>(x, y, width, height).toFloat();
+    auto knobRect = bounds.reduced(10.0f, 10.0f);
+    auto path = juce::Path();
+    
+    path.addEllipse(knobRect);
+    dropShadow.drawForPath(g, path);
+    
+    g.setColour(Colors::Knob::outline);
+    g.fillEllipse(knobRect);
+    
+    auto innerRect = knobRect.reduced(2.0f, 2.0f);
+    auto gradient = juce::ColourGradient(Colors::Knob::gradientTop,
+                                         0.0f,
+                                         innerRect.getY(),
+                                         Colors::Knob::gradientBottom,
+                                         0.0f,
+                                         innerRect.getBottom(),
+                                         false);
+    g.setGradientFill(gradient);
+    g.fillEllipse(innerRect);
+    
+    auto center = bounds.getCentre();
+    auto radius = bounds.getWidth();
+    auto lineWidth = 3.0f;
+    auto archRadius = radius - lineWidth / 2.0f;
+    
+    juce::Path backgroundArch;
+    backgroundArch.addCentredArc(center.x, 
+                                 center.y,
+                                 archRadius,
+                                 archRadius,
+                                 0.0f,
+                                 rotaryStartAngle,
+                                 rotaryEndAngle,
+                                 true);
+    
+    auto strokeType = juce::PathStrokeType(lineWidth,
+                                            juce::PathStrokeType::curved,
+                                            juce::PathStrokeType::rounded);
+    g.setColour(Colors::Knob::trackBackground);
+    g.strokePath(backgroundArch, strokeType);
+                                         
+    auto dialRadius = innerRect.getHeight() / 2.0f;
+    auto toAngle = rotaryStartAngle + sliderPosProportional * (rotaryEndAngle - rotaryStartAngle);
+    juce::Point<float> dialStart(center.x, center.y);
+    juce::Point<float> dialEnd(center.x + dialRadius * std::sin(toAngle),
+                               center.y - dialRadius * std::cos(toAngle));
+    
+    juce::Path dialPath;
+    dialPath.startNewSubPath(dialStart);
+    dialPath.lineTo(dialEnd);
+    
+    g.setColour(Colors::Knob::dial);
+    g.strokePath(dialPath, strokeType);
+}
+
+
+
+/**
+ {
     auto radius = (float) juce::jmin(width / 2, height / 2) - 4.0f;
     auto centreX = (float) x + (float) width  * 0.5f;
     auto centreY = (float) y + (float) height * 0.5f;
@@ -49,3 +114,4 @@ void KnobLookAndFeel::drawRotarySlider(juce::Graphics& g, int x, int y, int widt
     g.setColour(juce::Colours::lightblue);
     g.fillPath(p);
 }
+*/
